f6c68893686b06ceb605b2f55351b86af11fef47
Peeranat Na nakorn <mfkung_2@hotmail.com>
2017-09-15 14:02:27 +0700

    Add DIR_OFFSET

--- models.py
+++ models.py
@@ -1,20 +1,34 @@
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+ 
+DIR_OFFSET = { DIR_UP: (0,1),
+               DIR_RIGHT: (1,0),
+               DIR_DOWN: (0,-1),
+               DIR_LEFT: (-1,0) }
 class Snake:
+    BLOCK_SIZE = 16
     MOVE_WAIT = 0.2
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
         self.wait_time = 0
+        self.direction = DIR_UP
 
     def update(self, delta):
         self.wait_time += delta
  
         if self.wait_time < Snake.MOVE_WAIT:
             return
- 
         if self.x > self.world.width:
-            self.x = 0
-        self.x += 16
+            self.x = 0 
+        if self.y > self.world.height:
+            self.y = 0   
+        self.x += DIR_OFFSET[self.direction][0] * Snake.BLOCK_SIZE
+        self.y += DIR_OFFSET[self.direction][1] * Snake.BLOCK_SIZE
+        
         self.wait_time = 0
  
  
